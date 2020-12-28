4135c39feeb5c1c8e3266737f3e238a4f92eab88
SalaniTinko <muhammadarslantoor@gmail.com>
2020-12-28 14:13:25 +0500

    debug flase

--- sandev/settings.py
+++ sandev/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'zg+4lxvv@m6$3jl-nbl!k-&s-^n_d4f^p7istugttb481fr&_4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','muhammad-arslan-toor.herokuapp.com']
 
 
 # Application definition
